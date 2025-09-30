73e75eedcad847ce5afb47bbae768f9047a6b891
aleksandrakorol <aleksandrakorol7855@gmail.com>
2025-09-30 18:18:17 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -12,3 +12,42 @@ raw_data = pd.read_csv(File_url)
 print(raw_data.head(10))
 # выводим на экран первые 10 строк для проверки
 
+print(raw_data.info())
+# выводим информацию о данных датафрейма
+
+# приступаем к приведению типов
+
+# patient_id не требует приведения
+# lesion_id не требует приведения
+raw_data["smoke"] = raw_data["smoke"].astype("bool")
+raw_data["drink"] = raw_data["drink"].astype("bool")
+raw_data["background_father"] = raw_data["background_father"].astype("category")
+raw_data["background_mother"] = raw_data["background_mother"].astype("category")
+# age не требует приведения
+raw_data["pesticide"] = raw_data["pesticide"].astype("bool")
+raw_data["gender"] = raw_data["gender"].astype("category")
+raw_data["skin_cancer_history"] = raw_data["skin_cancer_history"].astype("bool")
+raw_data["cancer_history"] = raw_data["cancer_history"].astype("bool")
+raw_data["has_piped_water"] = raw_data["has_piped_water"].astype("bool")
+raw_data["has_sewage_system"] = raw_data["has_sewage_system"].astype("bool")
+# fitspatrick не требует приведения
+raw_data["region"] = raw_data["region"].astype("category")
+# diameter_1 не требует приведения
+# diameter_2 не требует приведения
+raw_data["diagnostic"] = raw_data["diagnostic"].astype("category")
+raw_data["itch"] = raw_data["itch"].astype("bool")
+raw_data["grew"] = raw_data["grew"].astype("bool")
+raw_data["hurt"] = raw_data["hurt"].astype("bool")
+raw_data["changed"] = raw_data["changed"].astype("bool")
+raw_data["bleed"] = raw_data["bleed"].astype("bool")
+raw_data["elevation"] = raw_data["elevation"].astype("bool")
+# img_id не требует приведения
+# biopsed не требует приведения
+
+print(raw_data.info())
+# выводим информацию о данных датафрейма после приведения типов
+
+raw_data.to_csv("conversion_data.csv", index=False)
+# сохраняем обработанные данные в формате csv
+
+
