5c6baed6e079b23e5cfff84bf862c65c0bb11156
aleksandrakorol <aleksandrakorol7855@gmail.com>
2025-10-19 22:27:14 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -21,8 +21,8 @@ print(raw_data.info())
 # lesion_id не требует приведения
 raw_data["smoke"] = raw_data["smoke"].astype("bool")
 raw_data["drink"] = raw_data["drink"].astype("bool")
-raw_data["background_father"] = raw_data["background_father"].astype("category")
-raw_data["background_mother"] = raw_data["background_mother"].astype("category")
+# background_father не требует приведения
+# background_mother не требует приведения
 # age не требует приведения
 raw_data["pesticide"] = raw_data["pesticide"].astype("bool")
 raw_data["gender"] = raw_data["gender"].astype("category")
@@ -31,16 +31,16 @@ raw_data["cancer_history"] = raw_data["cancer_history"].astype("bool")
 raw_data["has_piped_water"] = raw_data["has_piped_water"].astype("bool")
 raw_data["has_sewage_system"] = raw_data["has_sewage_system"].astype("bool")
 # fitspatrick не требует приведения
-raw_data["region"] = raw_data["region"].astype("category")
+# region не требует приведения
 # diameter_1 не требует приведения
 # diameter_2 не требует приведения
 raw_data["diagnostic"] = raw_data["diagnostic"].astype("category")
-raw_data["itch"] = raw_data["itch"].astype("bool")
-raw_data["grew"] = raw_data["grew"].astype("bool")
-raw_data["hurt"] = raw_data["hurt"].astype("bool")
-raw_data["changed"] = raw_data["changed"].astype("bool")
-raw_data["bleed"] = raw_data["bleed"].astype("bool")
-raw_data["elevation"] = raw_data["elevation"].astype("bool")
+raw_data["itch"] = raw_data["itch"].astype("category")
+raw_data["grew"] = raw_data["grew"].astype("category")
+raw_data["hurt"] = raw_data["hurt"].astype("category")
+raw_data["changed"] = raw_data["changed"].astype("category")
+raw_data["bleed"] = raw_data["bleed"].astype("category")
+raw_data["elevation"] = raw_data["elevation"].astype("category")
 # img_id не требует приведения
 # biopsed не требует приведения
 
@@ -49,6 +49,7 @@ print(raw_data.info())
 
 raw_data.to_parquet("conversion_data.parquet", index=False)
 # сохраняем обработанные данные в формате parquet
+
 
 
 
