32aee9ffc137489596707c47a073f1a2aff64756
aleksandrakorol <aleksandrakorol7855@gmail.com>
2025-11-01 14:45:54 +0300

    Update main.py

--- ETL/main.py
+++ ETL/main.py
@@ -2,8 +2,8 @@ import argparse
 import sys
 from pathlib import Path
 
-from .extract import load_scin_cancer_raw_data, validate_load_data, raw_data_save_csv
-from .transform import convert_types, clean_convert_data_from_NaN
+from .extract import load_scin_cancer_raw_data, raw_data_save_csv, ensure_dir
+from .transform import convert_types
 from .load import (
     save_data_parquet,
     validate_output,
@@ -18,38 +18,40 @@ def root() -> Path:
 
 
 def etl_run(place: str, table_name: str) -> None:
-    root()
+    ensure_dir(root())
 
     print("#### Запуск ETL процесса ####")
 
-    print("\nЗагрузка данных\n")
+    print("\n1. Загрузка данных\n")
     try:
         raw_data = load_scin_cancer_raw_data(place)
-        validate_raw = validate_load_data(raw_data)
-        save_raw_csv = raw_data_save_csv(raw_data, root)
+        save_raw = raw_data_save_csv(raw_data, root())
+        print("\nЗагузка прошла успешно!\n")
     except Exception as e:
         print(f"\nОшибка при загрузке данных: {e}")
         import traceback
+
         traceback.print_exc()
         sys.exit(1)
 
-    print("\nПреобразование данных\n")
+    print("\n2. Преобразование данных\n")
     try:
         conversation_data = convert_types(raw_data)
-        clean_data = clean_convert_data_from_NaN(conversation_data)
+        print("\nПреобразование прошло успешно!\n")
     except Exception as e:
         print(f"\nОшибка при преобразовании данных: {e}")
         import traceback
+
         traceback.print_exc()
         sys.exit(1)
 
-    print("\n Выгрузка датасета в базу данных\n")
+    print("\n3. Выгрузка датасета в базу данных\n")
     try:
-        save_parquet = save_data_parquet(clean_data, root)
-        validate_parquet = validate_output(clean_data)
+        save_parquet = save_data_parquet(conversation_data, root())
+        validate_parquet = validate_output(conversation_data)
         engine = get_connect_database()
-        load_to_db = load_dataset_in_database()
-        checking = check_table_in_database()
+        load_to_db = load_dataset_in_database(engine)
+        checking = check_table_in_database(engine)
     except Exception as e:
         print(f"\nОшибка при выгузке датасета: {e}")
         import traceback
